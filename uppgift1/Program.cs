1d7d081539131c0e9bf2aed2d911bee784b3e1ad
judenfors <rudenfors.eric@gmail.com>
2023-11-24 11:00:18 +0100

    olympiaden

---
+++ uppgift1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        Console.WriteLine("Ordet?");
+        string ordet = Console.ReadLine();
+
+        Console.WriteLine("Antal upprepningar?");
+        int rep = int.Parse(Console.ReadLine());
+
+        if(ordet.Length < 9 && rep < 9) 
+        {
+            Console.WriteLine("Svar: " + string.Concat(Enumerable.Repeat(ordet, rep)));
+        }
+
+        else
+        {
+            Console.WriteLine("För långt");
+        }
+    }
+}
